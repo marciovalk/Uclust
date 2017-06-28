4a2e6908ae612978c11f00e14691725c49f81275
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2017-06-28 15:40:23 -0300

    Add utest_classifiy: the same Uteste_class function, but with a new, more user-friendly interface.

---
+++ R/utest_classify.R
@@ -0,0 +1,64 @@
+#' Test for classification in one of two groups.
+#' 
+#' The null hypotheses is that the new data is not well classified in the first group and the 
+#' alternative hypotheses is that the data is well classified in the first group.
+#' 
+#' @param x A numeric vector to be classified.
+#' @param data A data.frame. Each row must be an observation and each column a dimension.
+#'   The last column must be the group factor with only two groups.
+#' @param groups A factor or character vector. It must contain only two elements: the first and second group names, respectively.
+#' @param numB Numeric scalar. The number of bootstraps. It's recommended 
+#'   \eqn{1000 < numB < 10000}.
+#' @return p-value based on bootstraps.
+#' @examples
+#' # Example 1
+#' # Five observations from each group, G1 and G2. Each observation has 60 dimensions.
+#' x <- matrix(c(rnorm(300, 0), rnorm(300, 10)), nrow = 60)
+#' data <- data.frame(t(x))
+#' data['group'] <- factor(rep(c('group1', 'group2'), each = 5))
+#' # Test data comes from G1.
+#' x <- rnorm(60, 0)
+#' # The test correctly indicates that the test data should be classified in G1 (p < 0.05).
+#' utest_classify(x, data, groups = c("group1", "group2"))
+#' 
+#' # Example 2
+#' # Five observations from each group, G1 and G2. Each observation has 60 dimensions.
+#' x <- matrix(c(rnorm(300, 0), rnorm(300, 10)), nrow = 60)
+#' data <- data.frame(t(x))
+#' data['group'] <- factor(rep(c('group1', 'group2'), each = 5))
+#' # Test data comes from G2.
+#' x <- rnorm(60, 10)
+#' # The test correctly indicates that the test data should be classified in G2 (p > 0.05).
+#' utest_classify(x, data, groups = c("group1", "group2"))
+#' @export
+utest_classify <- function(x, data, groups, bootstrap_iter = 1000)
+{
+    group_col <- ncol(data)
+    n_groups <- table(data[group_col])
+    ng1 <- unname(n_groups[groups[1]])
+    ng2 <- unname(n_groups[groups[2]])
+    
+    data <- data[order(data[group_col]), -group_col]
+    data <- rbind(data[1:ng1, ], x, data[(ng1+1):nrow(data), ])
+    distances <- as.matrix(dist(data))
+    B <- rep(0, bootstrap_iter)
+    B1_0 <- Bn(c(ng1 + 1, ng2), distances)
+    B2_0 <- Bn(c(ng1, 1 + ng2), distances)
+    
+    D = B1_0 - B2_0
+    
+    for (i in 1:bootstrap_iter)
+    {
+        vaux1 <- floor(runif(ng1, 1, ng1 + 1))
+        vaux2 <- floor(runif(ng2, ng1 + 1, ng2 + ng1 + 2))
+        vaux3 <- floor(runif(1, ng1 + 1, ng2 + ng1 + 2))
+        mataux <- distances[c(vaux1, vaux3, vaux2), c(vaux1, vaux3, vaux2)]
+        ngv1 <- c(ng1 + 1, ng2)
+        B1 <- Bn(ngv1, mataux)
+        ngv2 <- c(ng1, ng2 + 1)
+        B2 <- Bn(ngv2, mataux)
+        B[i] <- B1 - B2
+    }
+    
+    mean(B > D)
+}
